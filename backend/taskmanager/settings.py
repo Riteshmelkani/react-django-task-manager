56c5b67a882149dcebed453728effaacf7dd48d4
RiteshMelkani <riteshmelkani2004@gmail.com>
2026-06-11 00:38:05 +0530

    Add production server configuration

--- backend/taskmanager/settings.py
+++ backend/taskmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6_^1gjsav6+x^ty@z9%mcc*!v^6d8$gmrmbo57%14kl3y4*4ss
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
